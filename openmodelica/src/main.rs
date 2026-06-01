93debd283ad0872d7d8a282f23522b1227805545
Martin Sjölund <martin.sjolund@liu.se>
2026-06-01 08:42:29 +0200

    Fix checking of main

--- openmodelica/src/main.rs
+++ openmodelica/src/main.rs
@@ -1,6 +1,8 @@
-use openmodelica_backend::Main;
+use openmodelica_backend_main::Main;
 use std::env::args;
+use arcstr::ArcStr;
+use std::sync::Arc;
 
 fn main() -> () {
-    Main::main(args().collect())
+    Main::main(Arc::new(args().map(|e| ArcStr::from(e)).collect())).unwrap()
 }
