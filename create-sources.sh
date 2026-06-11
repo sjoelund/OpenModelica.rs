95304f7c776278b7055f1b2b70c9b27c24f00bc7
Martin Sjölund <martin.sjolund@liu.se>
2026-06-11 11:19:08 +0200

    Use the same omc everywhere

--- create-sources.sh
+++ create-sources.sh
@@ -3,7 +3,7 @@ RUST=$PWD
 /usr/bin/omc listMSL.mos
 mkdir -p /projects/OpenModelica/OMCompiler/Compiler/boot/parser
 cd /projects/OpenModelica/OMCompiler/Compiler/boot/parser
-omc -g=MetaModelica $RUST/listMetaModelicaFiles.mos
+/usr/bin/omc -g=MetaModelica $RUST/listMetaModelicaFiles.mos
 P=`realpath /projects/OpenModelica/OMCompiler/Compiler`
 sed -i "s,$P,/projects/OpenModelica/OMCompiler/Compiler," compilerSources.txt
 cp compilerSources.txt $RUST
